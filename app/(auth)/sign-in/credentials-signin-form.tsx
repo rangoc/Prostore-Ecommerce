d5392547f3a4b2e819f8e268b827e8b93e6c04e0
rangoc <cabarkapa.goran@gmail.com>
2024-12-26 17:05:53 +0100

    fix callbackUrl input type

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -31,7 +31,7 @@ const CredentialsSignInForm = () => {
   };
   return (
     <form action={action}>
-      <input type="hiden" name="callbackUrl" value={callbackUrl} />
+      <input type="hidden" name="callbackUrl" value={callbackUrl} />
       <div className="space-y-6">
         <div>
           <Label htmlFor="email">Email</Label>
